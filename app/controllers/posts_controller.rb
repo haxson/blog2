ace8e5b2433b202179a9056d179f1fc3e1adee0e
Mike Nichols <mhaxsec@gmail.com>
2016-05-26 03:27:07 -0700

    https://github.com/haxson/blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,34 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
-		def new 
-		end
+		def new
+		@post = Post.new
+	end
 
-		def create
-			@post = Post.new(post_params)
-			@post.save
+	def show
+		@post = Post.find(params[:id])
+	end
 
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
 			redirect_to @post
+		else
+			render 'new'
 		end
+	end
 
 		def show
 			@post = Post.find(params[:id])
 		end
-			
+
+			def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
 		
 		private 
 		def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments
+	validates :Title, presence: true, length: {minimum: 5}
+	validates :body, presence:true
 end
